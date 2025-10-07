aa4ab6167a3ce410c8063a0d5447d15132e48eba
robayet007 <mdrobayet007@gmail.com>
2025-10-07 16:06:08 +0600

    backend

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ import connectDb from "./config/db.js";
 import umrahRoutes from "./Routes/umrahRoutes.js"
 const server = express()
 const port = process.env.PORT || 5000;
-const database = process.env.database_url || "mongodb+srv://mohammadrobayet009_db_user:vkWbkhEYvOaGuv9i@cluster0.a9bbtmw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const database_url = process.env.database_url || "mongodb+srv://mohammadrobayet009_db_user:vkWbkhEYvOaGuv9i@cluster0.a9bbtmw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // dotenv.config(); // .env file load করবে
 // JSON parsing middleware
@@ -21,7 +21,7 @@ server.get("/", (req, res) => {
   res.send("Hello World!");
 })
 
-connectDb(database)
+connectDb(database_url)
 
 server.listen(port , () => {
     console.log(`server created successfully on port: ${port}`)
